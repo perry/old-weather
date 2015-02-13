ac92d8b65f1194569f208946d5d0096630398f3f
Perry Roper <perryroper@gmail.com>
2015-02-13 20:02:15 +0000

    Tidy up karma config.

--- karma.conf.js
+++ karma.conf.js
@@ -1,19 +1,7 @@
-// Karma configuration
-// Generated on Fri Feb 13 2015 16:17:11 GMT+0000 (GMT)
-
 module.exports = function(config) {
   config.set({
-
-    // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
-
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jasmine'],
-
-
-    // list of files / patterns to load in the browser
     files: [
         'app/bower_components/lodash/lodash.js',
         'app/bower_components/angular/angular.js',
@@ -24,50 +12,15 @@ module.exports = function(config) {
         'app/modules/*/scripts/**/!(init.js|*_test.js)',
         'app/modules/*/scripts/**/*_test.js'
     ],
-
-
-    // list of files to exclude
-    exclude: [
-    ],
-
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
         'app/modules/*/scripts/**/!(*_test.js)': 'coverage'
     },
-
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'coverage'],
-
-
-    // web server port
+    reporters: ['dots', 'coverage'],
     port: 9876,
-
-
-    // enable / disable colors in the output (reporters and logs)
     colors: true,
-
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
-
-
-    // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
-
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['Firefox'],
-
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
     singleRun: false
   });
 };
