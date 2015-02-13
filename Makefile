03fda63e4397a1559becd8a744bc47c0d4c6ad48
Perry Roper <perryroper@gmail.com>
2015-02-13 10:53:32 +0000

    Add deploy task.

--- Makefile
+++ Makefile
@@ -1,3 +1,24 @@
 install:
 	npm install
 	bower install
+
+build-app:
+	gulp build
+
+build-docs:
+	gulp docs
+
+sync:
+	s3cmd --delete-removed --acl-public --add-header="Cache-Control: no-cache" --config=.s3cfg sync $(source) $(dest)
+
+sync-app:
+	$(MAKE) sync source=./.tmp/build/* dest=s3://demo.zooniverse.org/oldweather/
+
+sync-docs:
+	$(MAKE) sync source=./.tmp/docs/* dest=s3://demo.zooniverse.org/oldweatherdocs/
+
+deploy-app: build-app sync-app
+
+deploy-docs: build-docs sync-docs
+
+deploy: deploy-app deploy-docs
--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,36 @@ var fs = require('fs');
 
 var gulp = require('gulp');
 var gulpDocs = require('gulp-ngdocs');
+var runSequence = require('run-sequence');
 var watch = require('gulp-watch');
 var clean = require('gulp-clean');
 var connect = require('gulp-connect');
 var templateCache = require('gulp-angular-templatecache');
 
+var usemin = require('gulp-usemin');
+var uglify = require('gulp-uglify');
+var minifyHtml = require('gulp-minify-html');
+var minifyCss = require('gulp-minify-css');
+var rev = require('gulp-rev');
+
 var baseDir = __dirname
 var appDir = baseDir + '/app';
 var modulesDir = appDir + '/modules';
 var files = {
-    templates: appDir + '/modules/**/templates/**/*.html'
+    templates: appDir + '/modules/**/templates/**/*.html',
+    scripts: modulesDir + '/**/*.js'
 }
 
 gulp.task('cleanTemplates', function () {
     return gulp.src('**/templates.js', {read: false}).pipe(clean());
+});
+
+gulp.task('cleanBuild', function () {
+    return gulp.src('.tmp/build').pipe(clean());
+});
+
+gulp.task('cleanDocs', function () {
+    return gulp.src('.tmp/docs').pipe(clean());
 });
 
 gulp.task('connectDev', function () {
@@ -30,6 +46,16 @@ gulp.task('connectDocs', function () {
         root: ['.tmp/docs'],
         port: 8001
     });
+});
+
+gulp.task('usemin', function () {
+    return gulp.src(appDir + '/index.html')
+    .pipe(usemin({
+        css: [minifyCss(), 'concat', rev()],
+        html: [minifyHtml({empty: true})],
+        js: [uglify(), rev()]
+    }))
+    .pipe(gulp.dest('.tmp/build/'));
 });
 
 gulp.task('templates', function () {
@@ -47,25 +73,55 @@ gulp.task('templates', function () {
     });
 });
 
+gulp.task('copy', function () {
+    gulp.src(appDir + '/images/**/*')
+        .pipe(gulp.dest('.tmp/build/images'))
+});
+
 gulp.task('ngdocs', function () {
     var options = {
-        html5Mode: false
+        html5Mode: false,
+        title: 'Old Weather Documentation'
     };
 
-    gulp.src(modulesDir + '/**/*.js')
+    gulp.src(files.scripts)
         .pipe(gulpDocs.process(options))
         .pipe(gulp.dest('.tmp/docs'));
 });
 
 gulp.task('watch', function () {
     gulp.watch([files.templates], ['templates']);
+    gulp.watch([files.scripts], ['ngdocs']);
 });
 
-gulp.task('default', [
-    'cleanTemplates',
-    'templates',
-    'ngdocs',
-    'watch',
-    'connectDev',
-    'connectDocs'
-]);
+gulp.task('default', function (cb) {
+    runSequence(
+        'cleanDocs',
+        'cleanTemplates',
+        'templates',
+        'ngdocs',
+        [
+            'watch',
+            'connectDev',
+            'connectDocs'
+        ],
+        cb
+    )
+});
+
+gulp.task('docs', function (cb) {
+    runSequence(
+        'cleanDocs',
+        'ngdocs',
+        cb
+    )
+});
+
+gulp.task('build', function (cb) {
+    runSequence(
+        'cleanBuild',
+        'copy',
+        'usemin',
+        cb
+    );
+});
