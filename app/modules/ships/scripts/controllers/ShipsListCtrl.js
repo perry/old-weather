badafab27246b2827a5de7c73ce038b95397e705
Perry Roper <perryroper@gmail.com>
2015-02-13 12:07:08 +0000

    Add sorting to the ships table.

--- app/modules/ships/scripts/controllers/ShipsListCtrl.js
+++ app/modules/ships/scripts/controllers/ShipsListCtrl.js
@@ -14,6 +14,25 @@
         '$scope',
         'shipsFactory',
         function ($scope, shipsFactory) {
+            $scope.headers = [
+                {name: '#', key: 'id'},
+                {name: 'Ship', key: 'name'},
+                {name: 'Difficulty', key: 'difficulty'},
+                {name: 'Crew', key: 'users'}
+            ];
+
+            $scope.columnSort = {key: $scope.headers[0].key, reverse: false};
+
+            $scope.sort = function (index) {
+                if ($scope.columnSort.key !== $scope.headers[index].key) {
+                    $scope.columnSort.reverse = false;
+                } else {
+                    $scope.columnSort.reverse = !$scope.columnSort.reverse;
+                }
+
+                $scope.columnSort.key = $scope.headers[index].key;
+            };
+
             shipsFactory.get()
                 .then(function (data) {
                     $scope.ships = data;
