df3b347aae5f92d7fb6d5934e08010d722587cdc
Roger Hutchings <roger@hutchin.gs>
2015-10-13 15:53:33 +0100

    Log out of Panoptes session as well as delete local auth data

--- app/modules/auth/scripts/init.js
+++ app/modules/auth/scripts/init.js
@@ -104,6 +104,7 @@
             localStorageService.set('auth', null);
             localStorageService.set('user', null);
             localStorageService.set('avatar', null);
+            zooAPI.auth.signOut();
             $rootScope.$broadcast('auth:signout');
         }
 
