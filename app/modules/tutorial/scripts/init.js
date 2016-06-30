b1058cdae6c8badb4ccab575c5d8080ff79952e8
Sascha T. Ishikawa <sascha@zooniverse.org>
2016-06-30 15:00:08 -0700

    begin stubbing out tutorial module

---
+++ app/modules/tutorial/scripts/init.js
@@ -0,0 +1,10 @@
+(function (angular) {
+    'use strict';
+
+    var module = angular.module('tutorial', []);
+
+    module.controller('TutorialController', ['$scope, $modal', function($scope, $modal) {
+      // $scope.greeting = 'Hola!';
+    }]);
+    
+}(window.angular));
