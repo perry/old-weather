80664c7360ed96d4f0957fa82527a6fde6beeff7
Sascha T. Ishikawa <sascha@zooniverse.org>
2016-09-14 19:14:01 -0700

    refactor directive to handle css positioning directly

--- app/modules/navTool/scripts/init.js
+++ app/modules/navTool/scripts/init.js
@@ -5,15 +5,33 @@
 
     module.controller('NavToolController', function($scope, $state, svgPanZoomFactory) {
       $scope.state = $state;
+
+      // set initial position
+      $scope.xPos = 400;
+      $scope.yPos = 400;
+
+      $scope.updatePosition = function(x,y) {
+        $scope.xPos = x;
+        $scope.yPos = y;
+      }
+
+      $scope.style = function() {
+        return {
+          left: $scope.xPos + 'px',
+          top: $scope.yPos + 'px'
+        };
+      }
+
     });
 
     module.directive('navTool', function ($document, svgPanZoomFactory) {
       return {
         restrict: 'E',
         templateUrl: 'templates/nav-tool.html',
+        controller: 'NavToolController',
         link: function (scope, element, attrs) {
 
-          var startX = 0, startY = 0, x = 20, y = 100;
+          var startX = 0, startY = 0, x = scope.xPos, y = scope.yPos;
 
           scope.onMouseDown = function(event) {
             event.preventDefault();
@@ -26,6 +44,7 @@
          function mousemove(event) {
            y = event.pageY - startY;
            x = event.pageX - startX;
+           scope.updatePosition(x,y);
            element.children(0).css({
              top: y + 'px',
              left: x + 'px'
